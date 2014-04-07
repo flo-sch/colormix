b56b485738772dc21620dbe06fd1eb4f9e3eb6d2
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2014-04-07 18:16:55 +0200

    Add force-download page for library files

--- examples/landing/index.php
+++ examples/landing/index.php
@@ -13,6 +13,7 @@ use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\SecurityServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\File;
 
 use Entea\Twig\Extension\AssetExtension;
 use Neutron\Silex\Provider\FilesystemServiceProvider;
@@ -53,9 +54,9 @@ $app->get('/documentation', function () use ($app) {
 })->bind('documentation');
 
 // Download
-$app->get('/download', function () use ($app) {
+$relativeBaseFolder = '../../build';
+$app->get('/download', function () use ($app, $relativeBaseFolder) {
     $files = array();
-    $relativeBaseFolder = '../../build';
     $basePath = __DIR__ . '/' . $relativeBaseFolder;
     $finder = new Finder();
     $finder->files()->in($basePath)->name('*.js')->sort(function (SplFileInfo $first, SplFileInfo $second) {
@@ -71,6 +72,23 @@ $app->get('/download', function () use ($app) {
         'files' => $files
     ));
 })->bind('download');
+
+$app->get('/get/{filename}', function($filename) use ($app, $relativeBaseFolder) {
+    $filename = __DIR__ . '/' . $relativeBaseFolder . '/' . $filename;
+    if ($app['filesystem']->exists($filename)) {
+        $file = new File($filename, true);
+        $fileInfos = new SplFileInfo($filename);
+        return $app->stream(function () use ($fileInfos) {
+            readfile($fileInfos->getRealPath());
+        }, 200, array(
+            'Content-Type' => $file->getMimeType(),
+            'Content-Length' => $fileInfos->getSize(),
+            'Content-Disposition' => 'attachment; filename="' . $fileInfos->getFilename() . '"'
+        ));
+    } else {
+        $app->abort(404, 'The file you are looking for cannot be found !');
+    }
+})->bind('get')->assert('filename', '[a-zA-Z0-9-_/.]*');
 
 // Demonstrations
 $app->get('/demonstrations', function () use ($app) {
